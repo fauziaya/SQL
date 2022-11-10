ba811ccbb72af06de4a302d0ba0d11e95029ebee
Fauzia Yumna <fauziaya25@gmail.com>
2022-11-10 14:37:51 +0700

    first commit

---
+++ Users Sales - Retail Dataset.sql
@@ -0,0 +1,22 @@
+*/
+	count the number of users per country
+/*
+SELECT country, 
+	COUNT(userid) number_of_user
+	FROM batch_19.fafa_usertab
+GROUP BY country
+
+
+*/
+	count the number of orders per country
+/*
+	
+	
+*/
+	count the number of orders per country
+/*
+	
+	
+*/
+	count the number of orders per country
+/*
