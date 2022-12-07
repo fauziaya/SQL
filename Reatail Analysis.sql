9e836e555fd1c952b7de727a01a7ff2576de8255
Fauzia Yumna <fauziaya25@gmail.com>
2022-12-07 14:38:02 +0700

    New

--- Reatail Analysis.sql
+++ Reatail Analysis.sql
@@ -3,7 +3,7 @@
 */
 SELECT country, 
 	   COUNT(userid) number_of_user
-FROM batch_19.fafa_usertab
+FROM user_tab_Users_Sales
 GROUP BY country
 
 
@@ -12,9 +12,11 @@ GROUP BY country
 */
 select u.country, 
 	   count(distinct o.orderid) number_of_order
-from batch_19.fafa_usertab u
-left join batch_19.fafa_ordertab o on u.userid = o.userid 
-group by 1	
+from user_tab_Users_Sales u
+left join order_tab_Users_Sales o 
+on u.userid = o.userid 
+group by 1
+order by 1 desc
 
 
 /* 
@@ -22,36 +24,56 @@ group by 1
 */
 select userid, 
 	   min(order_time) first_order_time
-from batch_19.fafa_ordertab
+from order_tab_Users_Sales
 group by 1
+order by 2 
 
 
 /* 
-	find the number of users who made their first order in each country and each day
+	find the number of users who made their first order 
+	in each country and each day
 */
 select o.first_order_time, 
 	   u.country, 
 	   count(distinct o.userid) total_users
 from (
 	select userid, min(order_time) first_order_time
-	from batch_19.fafa_ordertab
+	from order_tab_Users_Sales
 	group by 1
 	) o
-left join batch_19.fafa_usertab u on o.userid = u.userid 
+left join user_tab_Users_Sales u 
+on o.userid = u.userid 
 group by 1,2
 order by 1,2
 
 
 /* 
-	find the first order GMV of each user. If there is a tie, use the order with the lower orderid
+	find the first order GMV of each user. 
+	If there is a tie, use the order with the lower orderid
 */
 select userid,
 	   gmv 
 from (
-	select userid, gmv, min(order_time) first_order_time, min(orderid) first_order_id
-	from batch_19.fafa_ordertab
+	select userid, 
+	gmv,
+	min(order_time) first_order_time, 
+	min(orderid) first_order_id
+	from order_tab_Users_Sales
 	group by 1,2
+	order by 2
 	) o
+
+	
+/* 
+	count total gmv per country
+*/
+select u.country,
+count(o.gmv) total_gmv
+from user_tab_Users_Sales u
+left join order_tab_Users_Sales o 
+on u.userid = o.userid
+group by 1
+order by 2 desc
 
 
 /* 
@@ -62,7 +84,7 @@ from (
 	select itemid
 	from (
 		select itemid, gmv
-		from batch_19.fafa_ordertab
+		from order_tab_Users_Sales
 		group by 1,2
 	) o
 	group by 1
@@ -76,8 +98,9 @@ from (
 	find the user buy before register
 */
 select u.userid
-from batch_19.fafa_usertab u
-left join batch_19.fafa_ordertab o on u.userid = o.userid 
+from user_tab_Users_Sales u
+left join order_tab_Users_Sales o 
+on u.userid = o.userid 
 where o.order_time < u.register_time 
 
 */ There are many users buy before register */
@@ -87,7 +110,9 @@ where o.order_time < u.register_time
 	count the user buy before register
 */
 select count(distinct o.userid)
-from batch_19.fafa_usertab u
-right join batch_19.fafa_ordertab o on u.userid = o.userid 
+from user_tab_Users_Sales u
+right join order_tab_Users_Sales o 
+on u.userid = o.userid 
 
 */ There are 10.181 users exsist in order tab but did not in user tab */
+
